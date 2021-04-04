78b256b5933cf8b14909859e3c7c8217cf405e50
DESKTOP-3G40DR1\Diva <diva.lonial@gmail.com>
2021-04-04 14:30:59 +0800

    Automatic Publish

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-interface Colour {
-    red: number;
-    blue: number;
-    green: number;
-}
+export * from './errors/bad-request-error'
+export * from './errors/custom-error'
+export * from './errors/database-connection-error'
+export * from './errors/not-authorized-error'
+export * from './errors/not-found-error'
+export * from './errors/request-validation-error'
 
-const colour: Colour = {
-    red: 5,
-    blue: 10,
-    green: 10
-}
+export * from './middlewares/current-user'
+export * from './middlewares/error-handler'
+export * from './middlewares/require-auth'
+export * from './middlewares/validate-request'
 
-console.log(colour)
 
-export default colour;
